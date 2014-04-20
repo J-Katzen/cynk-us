a826ab348c7b80f07c0d6e40b6eaa300f80eb00f
Kwaku Farkye <kwafarkye@gmail.com>
2014-04-19 21:00:22 -0700

    subscribe to user also

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ var router    = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   console.log('Root URL');
-  console.log(res);
+  console.log(res.body());
   res.render('index', { title: 'Express' });
 });
 
 router.post('/', function(req, res) {
   console.log('Post to Root');
-  console.log(req);
+  console.log(req.body());
   consolel.lof('Response is: ')
-  console.log(res);
+  console.log(res.body());
 });
 
 router.get('/handleauth', function(req, res) {
