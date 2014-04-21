24bda4261e9d6a31066625720504102f8982671d
Kwaku Farkye <kwafarkye@gmail.com>
2014-04-21 00:44:08 -0700

    Revert "handleerror"
    This reverts commit c6ea9ab20ca84e5a7807f6d43cb6704ba984d2ca.

--- app.js
+++ app.js
@@ -153,8 +153,7 @@ var hsm = function handleStreamingMessages(jsonData){
                                 if(media.caption !== null)
                                     newMsg.set('message', media.caption.text);
                                 newMsg.save(function(err,savedMsg){
-                                    if(err) {}
-                                        // return handleError(err);
+                                    if(err) return handleError(err);
                                     console.log(savedMsg);
                                 });
                             }
