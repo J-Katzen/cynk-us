53bb19884719f4d9a8802c6b3f29835fe22af12f
Kwaku Farkye <kwafarkye@gmail.com>
2014-04-20 20:44:34 -0700

    move subscription stream

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express         =   require('express'),
-    favicon         =   require('static-favicon'),
-    logger          =   require('morgan'),
-    mongoose        =   require('mongoose'),
-    bodyParser      =   require('body-parser'),
-    cookieParser    =   require('cookie-parser'),
-    routes          =   require('./routes'),
-    methodOverride  =   require('method-override'),
-    InstagramStream =   require('instagram-realtime'),
-    http            =   require('http');
+var express             =   require('express'),
+    favicon             =   require('static-favicon'),
+    logger              =   require('morgan'),
+    mongoose            =   require('mongoose'),
+    bodyParser          =   require('body-parser'),
+    cookieParser        =   require('cookie-parser'),
+    routes              =   require('./routes'),
+    methodOverride      =   require('method-override'),
+    //InstagramStream     =   require('instagram-realtime'),
+    http                =   require('http'),
+    instagramStream     =   require('./instagram-subscription-stream');
+
 
 mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/cynkus');
 
@@ -16,23 +18,33 @@ var server  =   app.listen(process.env.PORT || 5000);
 var Establishment = require('./models/establishment');
 var DailyFeed = require('./models/dailyfeed');
 var Message = require('./models/message');
-var InstagramStream = require('instagram-realtime');
+//var InstagramStream = require('instagram-realtime');
 
 // Tells socket.io to user our express server
 var io      =   require('socket.io').listen(server);
 
 console.log('Process Env:' + process.env);
 
-// Create instagram stream
-var stream = InstagramStream(
-  server,
-  {
-    client_id     : "491c67def4b64d5d939abf92e6733f30",
-    client_secret : "76d396a74a4c4e208c558a8640ec6118",
-    url           : 'http://cynk-us.herokuapp.com',
-    callback_path : 'subscription'
-  }
-);
+///// Create instagram stream
+instagramStream.initStream(server);
+
+console.log("Initialized Instagram Stream");
+console.log('Value of test: ' + instagramStream.getTestVal());
+
+instagramStream.setTestVal(45);
+console.log('Value of test: ' + instagramStream.getTestVal());
+
+/////////////
+
+//var stream = InstagramStream(
+//  server,
+//  {
+//    client_id     : "kf491c67def4b64d5d939abf92e6733f30",
+//    client_secret : "kf76d396a74a4c4e208c558a8640ec6118",
+//    url           : 'http://cynk-us.herokuapp.com',
+//    callback_path : 'subscription'
+//  }
+//);
 
 // Subscribe to some things
 //stream.subscribe({ location : 214139311 });
@@ -54,14 +66,14 @@ var stream = InstagramStream(
 //stream.subscribe({ location : 795950 }); // Patxi's chicago pizza
 //stream.subscribe({ location : 1397980572 }); // Silver Cloud
 
-stream.subscribe({ tag: 'sfnight' });
-
-// Subscribe to SF Geography (max radius)
-stream.subscribe({ 
-    lat: 37.760, 
-    lng: -122.43953,
-    radius: 5000
-});
+//stream.subscribe({ tag: 'sfnight' });
+//
+//// Subscribe to SF Geography (max radius)
+//stream.subscribe({ 
+//    lat: 37.760, 
+//    lng: -122.43953,
+//    radius: 5000
+//});
 
 var fqh = function feedQueryHandler(err, feed){
     if(err) return handleError(err);
@@ -108,9 +120,9 @@ function handleStreamingMessages(jsonData){
                 feedQuery = DailyFeed.findOne({'created': {"$gte": today.getTime(), "$lt": new Date(today.getTime() + (24 * 60 * 60 * 1000))} });
                 console.log('after 6am');
             }
-            feedQuery.exec(fqh{
-                
-            });
+            //feedQuery.exec(fqh{
+            //    
+            //});
 
             messageQuery = Message.findOne({'instagramId': media.id});
             messageQuery.exec(function(err, msg){
@@ -145,48 +157,30 @@ function handleStreamingMessages(jsonData){
     });
 };
 
-stream.on('new', function(response, body) {
-    var jsonBody = JSON.parse(body);
-    var jsonData = jsonBody.data;
-    console.log(jsonData);
-    console.log('processing new media...: ' + String(jsonData.length));
-    handleStreamingMessages(jsonData);
-    console.log('parsed. :O!');
-});
-
-stream.on('new/error', function(response, body) {
-    console.log("New Media Error");
-});
-
-stream.on('subscribe', function(response, body) {
-    console.log("Subscribed on Instagram");
-});
-
-stream.on('subscribe/error', function (error, response, body) {
-    console.log("Error" + body);
-});
-
-stream.on('unsubscribe', function(response, body) {
-    console.log("Unsubscribed, so resubscribe");
-    stream.subscribe({ location : 214139311 });
-    stream.subscribe({ location : 258180411 });
-    stream.subscribe({ location : 13216121 });
-    stream.subscribe({ location : 47125 });
-    stream.subscribe({ location : 7908534 });
-    stream.subscribe({ location : 5616777 });
-    stream.subscribe({ location : 75697550 });
-    stream.subscribe({ location : 1438 });
-    stream.subscribe({ location : 213863948 });
-    stream.subscribe({ location : 215620527 });
-    stream.subscribe({ location : 1333 }); // Smuggler's Cove
-    stream.subscribe({ location : 82756577 }); // Smuggler's Cove mid floor
-    stream.subscribe({ location : 5882549 }); // The Ice Cream Bar
-    stream.subscribe({ location : 118516 }); // The Boardroom
-    stream.subscribe({ location : 11945887 }); // Board Room
-    stream.subscribe({ location : 365453 }); // Curly's Coffee Shop
-    stream.subscribe({ location : 795950 }); // Patxi's chicago pizza
-    stream.subscribe({ location : 1397980572 }); // Silver Cloud
-})
+//stream.on('new', function(response, body) {
+//    var jsonBody = JSON.parse(body);
+//    var jsonData = jsonBody.data;
+//    console.log(jsonData);
+//    console.log('processing new media...: ' + String(jsonData.length));
+//    handleStreamingMessages(jsonData);
+//    console.log('parsed. :O!');
+//});
+//
+//stream.on('new/error', function(response, body) {
+//    console.log("New Media Error");
+//});
+//
+//stream.on('subscribe', function(response, body) {
+//    console.log("Subscribed on Instagram");
+//});
+//
+//stream.on('subscribe/error', function (error, response, body) {
+//    console.log("Error while subscribing: " + body);
+//});
+//
+//stream.on('unsubscribe', function(response, body) {
+//    console.log("Unsubscribed, so resubscribe");
+//});
 
 
 // SocketIO server
